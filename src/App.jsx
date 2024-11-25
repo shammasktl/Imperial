38459c4756f224da76b3a4af821c06eef12b9871
shammasktl <mshammast370737@gmail.com>
2024-11-25 10:30:55 +0530

    Added Libraries

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,11 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Academics from './Pages/Academics/Academics'
-import Login from './Pages/Login/Login'
-import Gallery from './Pages/Gallery/Gallery'
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/gallery' element={<Gallery />} />
-        <Route path='/academics' element={<Academics />} />
-        <Route path='/authenticate' element={<Login />} />
       </Routes>
       <Footer />
     </>
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { main } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='logo'>
+      <img src={main} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
